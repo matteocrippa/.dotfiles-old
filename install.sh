116ce27d0fc3b4cf886f54a2704a93a4a05bbf0e
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-06-19 22:08:33 +0200

    add printer

--- install.sh
+++ install.sh
@@ -33,3 +33,21 @@ full() {
         yay -Sy $pkg --needed --noconfirm
     done
 }
+
+# printer install
+while true; do
+    read -p 'do you want to install a printer y or n?' printer
+
+    case $printer in
+        [yY]* ) install_printer; break;;
+        [nN]* ) break;;
+        * ) echo 'Plase answer y or n';;
+    esac
+done
+
+install_printer() {
+    yay -Sy sane brother-dcp1610w cups brscan4 simple-scan-git system-config-printer
+    sudo brsaneconfig4 -a name="Brother" model="DCP1610W" ip=192.168.0.16
+    sudo systemctl enable org.cups.cupsd.service
+    sudo systemctl start org.cups.cupsd.service
+}
