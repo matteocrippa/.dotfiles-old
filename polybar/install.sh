005463065e6d6b84b1bbffc31fbdf8bca35b3757
Matteo Crippa <matteo.crippa+dev@gmail.com>
2019-06-22 14:45:54 +0200

    improved script

--- polybar/install.sh
+++ polybar/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm ./battery-combined-udev.sh
+rm battery-combined-udev.sh
 
-if [ -f /sys/class/power_supply/AC]; then
-    mv ./battery-lenovo.sh ./battery-combined-udev.sh
+if [ -f /sys/class/power_supply/AC ]; then
+    cp battery-lenovo.sh battery-combined-udev.sh
 else
-    mv ./battery-gpd.sh ./battery-combined-udev.sh
+    cp battery-gpd.sh battery-combined-udev.sh
 fi
