bde98e799f70b6d28bafa75566a45f8b7711123c
matteocrippa <matteocrippa@users.noreply.github.com>
2019-06-24 17:46:55 +0200

    add i3scrit

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/.i3
-rm -Rf ~/.i3/config
+rm -Rf ~/.i3
 rm -Rf ~/.config/i3
-ln -sfT ~/.dotfiles/i3/config ~/.i3/config
+mkdir -p ~/.config/i3
+ln -sfT ~/.dotfiles/i3/config ~/.config/i3/config
+ln -sfT ~/.dotfiles/i3/i3scrot.conf ~/.config/i3/i3scrot.conf
