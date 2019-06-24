825a488568a8c225a2b096d86dfe4cc68827e23f
matteocrippa <matteocrippa@users.noreply.github.com>
2019-06-24 11:02:10 +0200

    fix check for lenovo vs gdp

--- polybar/install.sh
+++ polybar/install.sh
@@ -2,8 +2,10 @@
 
 rm battery-combined-udev.sh
 
-if [ -f /sys/class/power_supply/AC ]; then
+if [ -d /sys/class/power_supply/AC ]; then
+    echo 'Lenovo'
     cp battery-lenovo.sh battery-combined-udev.sh
 else
+    echo 'GPD'
     cp battery-gpd.sh battery-combined-udev.sh
 fi
