e84f9547ffa35034daec7f1fdc9cd6aec778f548
matteocrippa <matteocrippa@users.noreply.github.com>
2019-07-02 12:46:56 +0200

    fix ip

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ done
 # printer install
 install_printer() {
     yay -Sy sane brother-dcp1610w cups brscan4 simple-scan-git system-config-printer --needed --noconfirm
-    sudo brsaneconfig4 -a name="Brother" model="DCP1610W" ip=192.168.0.16
+    sudo brsaneconfig4 -a name="Brother" model="DCP1610W" ip=192.168.0.11
     sudo systemctl enable org.cups.cupsd.service
     sudo systemctl start org.cups.cupsd.service
 }
