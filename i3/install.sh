4870727e1f1057c26dff0ba0e7733948cc8df6a8
matteocrippa <matteocrippa@users.noreply.github.com>
2019-07-06 23:14:05 +0200

    fix script for i3 scrot install

--- i3/install.sh
+++ i3/install.sh
@@ -4,4 +4,5 @@ rm -Rf ~/.i3
 rm -Rf ~/.config/i3
 mkdir -p ~/.config/i3
 ln -sfT ~/.dotfiles/i3/config ~/.config/i3/config
-ln -sfT ~/.dotfiles/i3/i3scrot.conf ~/.config/i3/i3scrot.conf
+rm -Rf ~/.config/i3-scrot.conf
+ln -sfT ~/.dotfiles/i3/i3-scrot.conf ~/.config/i3-scrot.conf
