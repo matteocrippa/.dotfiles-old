3a5bca3c889f7993f067a437a83a477f4b5f1267
matteocrippa <matteocrippa@users.noreply.github.com>
2019-07-21 15:57:21 +0200

    add exa instead of ls

--- packages.sh
+++ packages.sh
@@ -14,6 +14,7 @@ export LIGHT=(
   rofi
   rofi-calc
   xautolock
+  arc-gtk-theme
 
   # utils
   unzip
@@ -32,6 +33,7 @@ export LIGHT=(
   wavemon
   cpupower
   zip
+  exa
 
   # apps x
   firefox
