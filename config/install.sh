103ad139de605d220b75222e65dffbb6ebda072a
matteocrippa <matteocrippa@users.noreply.github.com>
2019-07-23 11:13:51 +0200

    updated tlp

--- config/install.sh
+++ config/install.sh
@@ -9,3 +9,9 @@ ln -sfT ~/.dotfiles/config/alacritty ~/.config/alacritty
 yay -Sy nerd-fonts-complete ttf-font-awesome noto-fonts-emoji --needed --noconfirm
 
 sudo ln -sfT ~/.dotfiles/polybar/95-battery.rules /etc/udev/rules.d/
+
+# laptop mode
+sudo ln -sfT ~/.dotfiles/config/tlp /etc/default/tlp
+sudo systemctl enable tlp
+sudo systemctl enable tlp-sleep.service
+sudo systemctl enable thermald
