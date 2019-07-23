eb95b7b8e7db2d6b416896c81ab38a3196550b36
matteocrippa <matteocrippa@users.noreply.github.com>
2019-07-23 17:08:41 +0200

    fix refind script:wq:

--- scripts/efi-theme.sh
+++ scripts/efi-theme.sh
@@ -1,3 +1,3 @@
 mkdir /boot/EFI/refind/themes
-sudo git clone https://github.com/EvanPurkhiser/rEFInd-minimal.git /boot/EFI/refind/themes/minimal
-sudo echo "include themes/minimal/theme.conf" | sudo tee -a /boot/EFI/refind/refind.conf
+sudo git clone https://github.com/EvanPurkhiser/rEFInd-minimal.git /boot/EFI/refind/themes/rEFInd-minimal
+sudo echo "include themes/rEFInd-minimal/theme.conf" | sudo tee -a /boot/EFI/refind/refind.conf
