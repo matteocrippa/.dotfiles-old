d2adabceabba99943082a4229b43d3ef29333b88
matteocrippa <matteocrippa@users.noreply.github.com>
2019-07-30 15:38:17 +0200

    updated scripts

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -2,6 +2,6 @@ yay -Sy virtualbox virtualbox-host-modules-arch
 sudo modprobe vboxdrv
 sudo gpasswd -a matteo vboxusers
 yay -Sy virtualbox-ext-oracle
-git clone git@github.com:myspaghetti/macos-guest-virtualbox.git ~/vbox
-cd ~/vbox
+git clone git@github.com:myspaghetti/macos-guest-virtualbox.git ~/VirtualBox\ VMs/vbox
+cd ~/VirtualBox\ VMs/vbox
 sh macos-guest-virtualbox.sh
