e500a59968c270f81277926e9b600e18c276081d
matteocrippa <matteocrippa@users.noreply.github.com>
2019-08-02 11:11:04 +0200

    updated dock script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -13,8 +13,8 @@ EOF
 function change_screen_to() {
   case $1 in
   dock)
-    xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --mode 2560x1080 --pos 0x0 --rotate normal --output     HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
-    ;;
+     xrandr --output eDP1 --primary --mode 1920x1080 --pos 1663x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --mode 2560x1080 --pos 0x0 --rotate normal --output DP2-3 --mode 2560x1080 --pos 2560x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+      ;;
   laptop)
     xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
     ;;
